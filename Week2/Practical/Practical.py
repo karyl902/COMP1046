bff4b8b59f4a15f90a9bbc56f0119d40e8af7b10
karyl902 <521332@learning.eynesbury.edu.au>
2025-02-24 10:36:58 +1030

    11121

--- Week2/Practical/Practical.py
+++ Week2/Practical/Practical.py
@@ -1,2 +1,9 @@
 class Movie:
-    pass
+    def __init__(self, name, genre, minutes, rating):
+        self.name = name
+        self.genre = genre
+        self.minutes = minutes
+        self.rating = rating
+action = Movie("Last Action Hero", "Action", 130, 'PG')
+print(action)
+# OUTPUT>>> <__main__.Movie object at 0x01BA1FB8>
