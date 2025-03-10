6f10de77c1b837d40836feea4ab62c7e275ff761
karyl902 <521332@learning.eynesbury.edu.au>
2025-03-10 11:04:40 +1030

    112

--- Week3/Practical/practical.py
+++ Week3/Practical/practical.py
@@ -1,3 +1,4 @@
+#Task1
 class Card:
     def __init__(self, suit, value_str):
         self.suit = suit
@@ -25,55 +26,5 @@ print(c1.toString(), 'is worth', c1.getValue())
 
 c2 = Card('spades', 'jack')
 print(c2.toString(), 'is worth', c2.getValue())
-#Task2
-import random
 
-class Deck:
-    def __init__(self):
-        self.cards = []
-        self.reset()
 
-    def reset(self):
-        suits = ['hearts', 'spades', 'diamonds', 'clubs']
-        values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
-        self.cards = []
-        for suit in suits:
-            for value in values:
-                new_card = Card(suit, value) 
-                self.cards.append(new_card)
-
-    def shuffle(self):
-        random.shuffle(self.cards) 
-
-    def drawCard(self):
-        if len(self.cards) > 0:
-            return self.cards.pop() 
-        else:
-            print("Deck is empty")
-            return None
-class Hand:
-    def __init__(self):
-        self.cards = [] 
-
-    def addToHand(self, card):
-        if len(self.cards) < 5:
-            self.cards.append(card) 
-        else:
-            print("Hand is full. Cannot add more cards.")
-
-    def getHandValue(self):
-        total = 0
-        for card in self.cards:
-            total += card.getValue() 
-        return total
-
-    def emptyHand(self):
-        self.cards = [] 
-
-    def showCards(self):
-        output = ""
-        for i in range(len(self.cards)):
-            output += self.cards[i].toString()
-            if i != len(self.cards) - 1:
-                output += ", "
-        print(output) 
