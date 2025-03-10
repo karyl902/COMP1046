4f55d2617d78608573058316f00b63920947bd19
karyl902 <521332@learning.eynesbury.edu.au>
2025-03-10 11:44:09 +1030

    112

--- Week3/Practical/practical.py
+++ Week3/Practical/practical.py
@@ -1,4 +1,3 @@
-#Task1
 class Card:
     def __init__(self, suit, value_str):
         self.suit = suit
@@ -26,5 +25,73 @@ print(c1.toString(), 'is worth', c1.getValue())
 
 c2 = Card('spades', 'jack')
 print(c2.toString(), 'is worth', c2.getValue())
+#Task2
+import random
 
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.reset()
 
+    def reset(self):
+        suits = ['hearts', 'spades', 'diamonds', 'clubs']
+        values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+        self.cards = []
+        for suit in suits:
+            for value in values:
+                new_card = Card(suit, value) 
+                self.cards.append(new_card)
+
+    def shuffle(self):
+        random.shuffle(self.cards) 
+
+    def drawCard(self):
+        if len(self.cards) > 0:
+            return self.cards.pop() 
+        else:
+            print("Deck is empty")
+            return None
+deck = Deck()
+print("Initial deck size:", len(deck.cards)) 
+print("First card drawn:", deck.drawCard().toString()) 
+deck.shuffle()
+print("Shuffled card:", deck.drawCard().toString()) 
+deck.reset()
+print("Deck reset to size:", len(deck.cards))
+deck.reset()
+#Task3
+class Hand:
+    def __init__(self):
+        self.cards = [] 
+
+    def addToHand(self, card):
+        if len(self.cards) < 5:
+            self.cards.append(card) 
+        else:
+            print("Hand is full. Cannot add more cards.")
+
+    def getHandValue(self):
+        total = 0
+        for card in self.cards:
+            total += card.getValue() 
+        return total
+
+    def emptyHand(self):
+        self.cards = [] 
+
+    def showCards(self):
+        output = ""
+        for i in range(len(self.cards)):
+            output += self.cards[i].toString()
+            if i != len(self.cards) - 1:
+                output += ", "
+        print(output) 
+
+hand = Hand()
+hand.addToHand(deck.drawCard()) 
+hand.addToHand(deck.drawCard())
+print("Hand cards:")
+hand.showCards()
+print("Hand value:", hand.getHandValue()) 
+hand.emptyHand()
+print("Hand value after empty:", hand.getHandValue()) 
