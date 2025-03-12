15716b507d9a966645cf387f43f05ce160a3b08f
karyl902 <521332@learning.eynesbury.edu.au>
2025-03-13 10:16:03 +1030

    111

---
+++ Week4/Tutorial/tut1.py
@@ -0,0 +1,21 @@
+class Vehicle:
+    def __init__(self):
+        self.reverse = False
+        pass
+    def start_engine(self):
+        return
+    def stop_engine(self):
+        return
+    def move(self):
+        return
+    def turn_left(self):
+        return
+    def turn_right(self):
+        return
+    def accelerate(self):
+        return
+    def decelerate(self):
+        return
+    def change_reverse(self):
+        return
+    pass  
