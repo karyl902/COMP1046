ee1def51faa84b2625132c72ebe2196fac4fb133
karyl902 <521332@learning.eynesbury.edu.au>
2025-03-13 12:02:40 +1030

    1213

--- Week4/Tutorial/Tut.py
+++ Week4/Tutorial/Tut.py
@@ -1,4 +1,19 @@
 from vehicle import Vehicle  # type: ignore
 
 class Aircraft(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+    def __str__(self):
+        return "aircraft"
+        #return super().__str__()
+    #def __name__(self):
+        #return super().name()
+    def move_up(self):
+        print("move up")
+        return
+    def move_down(self):
+        print("move down")
+        return
+    
     pass 
--- Week4/Tutorial/tut1.py
+++ Week4/Tutorial/tut1.py
@@ -1,21 +1,32 @@
 class Vehicle:
     def __init__(self):
-        self.reverse = False
         pass
+    def __str__(self):
+        return "vehicle"
+    def __name__(self):
+        return self.__str__()
     def start_engine(self):
+        print("start engine")
         return
     def stop_engine(self):
+        print("stop engine")
         return
-    def move(self):
+    def move_forward(self):
+        print("move forward")
+        return
+    def move_back(self):
+        print("move back")
         return
     def turn_left(self):
+        print("turn left")
         return
     def turn_right(self):
+        print("turn right")
         return
     def accelerate(self):
+        print("accelerate")
         return
     def decelerate(self):
-        return
-    def change_reverse(self):
+        print("decelerate")
         return
     pass  
