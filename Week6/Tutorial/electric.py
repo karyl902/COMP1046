7ee8686566909ef254edefba9626c28242d05a07
karyl902 <521332@learning.eynesbury.edu.au>
2025-03-30 23:20:31 +0000

    121

--- Week6/Tutorial/electric.py
+++ Week6/Tutorial/electric.py
@@ -3,5 +3,18 @@
 #Student ID: 521332
 #Email: 521332@learning.eynesbury.edu.au
 #This program is my own work as defined by the Academic Integrity
-class electric:
-    pass
+from battery import Battery
+from engine import Engine  
+class ElectricEngine(Engine):
+    def __init__(self, battery_capacity):
+        super().__init__()
+        self.battery = Battery(battery_capacity)  
+    
+    def start(self):
+        if self.battery.level > 0:
+            print("Electric engine started.")
+        else:
+            print("Battery level too low to start.")
+    
+    def charge_engine(self, amount):
+        self.battery.charge(amount)  
