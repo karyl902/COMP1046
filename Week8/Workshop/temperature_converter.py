be70d897ab1c95022397310f91134cdd7726aae4
karyl902 <521332@learning.eynesbury.edu.au>
2025-04-14 01:12:20 +0000

    Initial import of files.

---
+++ Week8/Workshop/temperature_converter.py
@@ -0,0 +1,8 @@
+
+
+def f_to_c(f):
+    '''Convert argument temperature from fahrenheit to celsius.'''
+    if (isinstance(f, int) or isinstance(f, float)):
+        return (f-32)*(5/9)
+    else:
+        raise TypeError("Only int or float is allowed.")
