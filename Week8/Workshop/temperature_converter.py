a34b9c42c50b70e4b375137d779557b50b86d44e
karyl902 <521332@learning.eynesbury.edu.au>
2025-04-14 01:17:00 +0000

    Implemented c_to_f function

--- Week8/Workshop/temperature_converter.py
+++ Week8/Workshop/temperature_converter.py
@@ -6,3 +6,9 @@ def f_to_c(f):
         return (f-32)*(5/9)
     else:
         raise TypeError("Only int or float is allowed.")
+
+def c_to_f(c):
+    if (isinstance(c, int) or isinstance(c, float)):
+        return (9 / 5) * c + 32
+    else:
+        raise TypeError("Only int or float is allowed.")
